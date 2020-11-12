b1bfd708b23d998a737317fd405044aaac5e7ca9
Cody Frick <frick_95@yahoo.com>
2020-11-11 21:27:57 -0500

    Bot presence is now properly set if proper config settings are supplied.

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,21 @@
 module.exports = (client, message) => {
     console.log(`Logged in as: ${client.user.tag}`);
+    
+    var statusType = client.config.statusType;
+    var statusMessage = client.config.statusMessage;
+    var statusUrl = client.config.statusUrl;
 
-    client.user.setPresence({
-        status: "online",
-        activity: {
-            name: client.config.statusMessage
+    client.user.setPresence(
+        { 
+            status: 'online',
+            game: 
+            { 
+                type: statusType, 
+                name: statusMessage,
+                url: statusUrl
+            }
         }
-    });
+    )
+    .then(console.log)
+    .catch(console.error);
 };
